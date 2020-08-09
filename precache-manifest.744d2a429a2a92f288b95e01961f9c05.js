7366d5e8cc2bde9799fbe165b8f5cda30ae1c9e8
biem <bellomaryam@ymail.com>
2020-08-09 20:29:49 +0100

    Updates

---
+++ precache-manifest.744d2a429a2a92f288b95e01961f9c05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f0e98b688b671e92b29597c3331fce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0490d3af23f34c02c2ac",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1718442a19992e9f8db5",
+    "url": "/static/css/main.c7a6b96d.chunk.css"
+  },
+  {
+    "revision": "0490d3af23f34c02c2ac",
+    "url": "/static/js/2.8e3d7a34.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8e3d7a34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1718442a19992e9f8db5",
+    "url": "/static/js/main.1c6278e1.chunk.js"
+  },
+  {
+    "revision": "393c07e22e0d573f3ec1",
+    "url": "/static/js/runtime-main.e4b50e06.js"
+  }
+]);
